5b4eca32322114b0a2de3ca981462834196989d1
Ravi Prakash <1907prakash.ravi@gmail.com>
2021-03-28 10:14:00 +0530

    debugging client

--- Submissions/project1/Secreat Share/shareBox/client.py
+++ Submissions/project1/Secreat Share/shareBox/client.py
@@ -1,6 +1,4 @@
-"""
-Python code file for Client Side executables
-"""
+"""   Client/Receiver Side   """
 
 import os
 import json
@@ -18,9 +16,10 @@ class client:
 
 		# Input Hostname and define Port
 		self.host = input("Host: ")
-		self.port = 55555 #8080
+		self.port = 55555
 
 		# Binding socket with the host and port
+		self.connected = False
 		try:
 			self.sock_client.connect((self.host, self.port))
 			try:
@@ -28,7 +27,9 @@ class client:
 				print("Connected...")
 			except:
 				print("Connected...")
-		except ConnectionRefusedError:
+			self.connected = True
+
+		except:
 			print("Currently server is down!! Try later!")
 
 	def formatFileName(self, gotName):
@@ -39,35 +40,34 @@ class client:
 		return gotName
 
 	def client(self):
+		if self.connected:		
+			# Receiving the Header for the Main Data
+			detail_header = self.sock_client.recv(CHUNK)
+
+			# Loading the Json Data from the header received
+			detail_header = json.loads(detail_header)
+			print(detail_header)
+
+			for i in range(1, len(detail_header) + 1):
+				filename = self.formatFileName(detail_header[str(i)]['filename'])
+
+				# Opening new file to write the receiving data
+				with open(filename, 'wb') as f:
+					print("saving ",filename)
+
+					# Writing the received data to the file opened
+					# Checking If the file size exceeds the original file size
+					while detail_header[str(i)]['filesize'] > f.tell():
+						file_data = self.sock_client.recv(CHUNK)
+						if not file_data:	# Checking if the File Ends
+							break
+						f.write(file_data)
+					print(f.tell())
+
+				f.close()
+
+			print("File(s) received...")
 		
-		# Receiving the Header for the Main Data
-		detail_header = self.sock_client.recv(CHUNK)
-
-		# Loading the Json Data from the header received
-		detail_header = json.loads(detail_header)
-		print(detail_header)
-
-		for i in range(1, len(detail_header) + 1):
-			filename = self.formatFileName(detail_header[str(i)]['filename'])
-
-			# Opening new file to write the receiving data
-			with open(filename, 'wb') as f:
-				print("saving ",filename)
-
-				# Writing the received data to the file opened
-				# Checking If the file size exceeds the original file size
-				#while detail_header[str(i)]['filesize'] > f.tell():
-				while detail_header[str(i)]['filesize'] > f.tell():
-					file_data = self.sock_client.recv(CHUNK)
-					if not file_data:	# Checking if the File Ends
-						print("Here")
-						break
-					f.write(file_data)
-				print(f.tell())
-
-			f.close()
-
-		print("File has been received...")
 
 		###########
 		self.sock_client.close()	# Closing the Connection
